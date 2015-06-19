f4ccc4919e21b8e2a183ca2585c3fdaa967fc3e7
Dave Mueller <dmueller@liquidweb.com>
2015-06-19 09:30:50 -0400

    Fixing another routing issue

--- cluster/vagrant/provision-network.sh
+++ cluster/vagrant/provision-network.sh
@@ -70,7 +70,7 @@ function provision-network {
     NODE_INDEX=0
     for remote_ip in "${CONTAINER_IPS[@]}"
     do
-        if [ "\${remote_ip}" == "${NODE_IP}" ]; then
+        if [ "${remote_ip}" == "${NODE_IP}" ]; then
              ovs-ofctl -O OpenFlow13 add-flow ${OVS_SWITCH} "table=0,ip,in_port=10,nw_dst=${CONTAINER_SUBNETS[${NODE_INDEX}]},actions=output:9"
              ovs-ofctl -O OpenFlow13 add-flow ${OVS_SWITCH} "table=0,arp,in_port=10,nw_dst=${CONTAINER_SUBNETS[${NODE_INDEX}]},actions=output:9"
         else
@@ -86,6 +86,6 @@ function provision-network {
     ip route add ${CONTAINER_SUBNET} dev ${DOCKER_BRIDGE} scope link src ${CONTAINER_ADDR}
   }
   echo "Network configuration verified"
-  ovs-vsctl set bridge ${OVS_SWITCH} protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13
-  echo "Reset bridge version"
+  #ovs-vsctl set bridge ${OVS_SWITCH} protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13
+  #echo "Reset bridge version"
 }
